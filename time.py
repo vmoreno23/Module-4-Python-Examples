c7971bd78b113607382d0c395939df1cf62b3bef
vmoreno23 <123032245+vmoreno23@users.noreply.github.com>
2024-02-09 20:36:27 -0600

    Add files via upload

---
+++ time.py
@@ -0,0 +1,8 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait"
+
+currentTimeInt = int(current_time_str)
+waitTimeInt = int(wait_time_str)
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTime_Int)
---
+++ time2.py
@@ -0,0 +1,7 @@
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of nours to wait?")
+time = int(str_time)
+wai_time = int(str_wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
