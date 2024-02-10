ea57d2a36e9d5a48b581da0a52c154f98cf4a5af
vmoreno23 <123032245+vmoreno23@users.noreply.github.com>
2024-02-09 20:51:10 -0600

    Update pirate.py

--- pirate.py
+++ pirate.py
@@ -1,5 +1,8 @@
 greeting = input("Hello, possible pirate! What's the password?")
-if greeting in ["Arrr!"]:
+if greeting in ["Arrr!"]: #if message is Arrr!
 	print("Go away, pirate.")
 else:
     print("Greetings, hater of pirates!")
+
+#Victor Moreno
+#2/8/24
